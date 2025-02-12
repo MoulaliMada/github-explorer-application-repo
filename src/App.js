84ef7bd7b0710e0de8b55d3ebaf8c38fdb668887
moulali <moulalimoula190@gmail.com>
2025-02-12 17:18:29 +0530

    Fetching data and display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import HomeScreen from './components/HomeScreen';
+ import DetailsScreen from './components/DetailsScreen';
+ import FavoritesScreen from './components/FavoritesScreen';
+//import { ThemeProvider } from './context/ThemeContext';
+//import './styles/App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      {/*<ThemeProvider> */}
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/details/:id" element={<DetailsScreen />} />
+            <Route path="/favorites" element={<FavoritesScreen />} /> 
+          </Routes>
+        </Router>
+      {/* </ThemeProvider> */}
+    </Provider>
   );
 }
-
-export default App;
---
+++ src/components/HomeScreen/index.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchRepositories } from "../../redux/actions";
+import Header from "../Header";
+import './index.css'
+
+function HomeScreen() {
+  const [query, setQuery] = useState("");
+  const dispatch = useDispatch();
+  const repositories = useSelector((state) => state.repositories);
+
+  useEffect(() => {
+    if (query === "") {
+      dispatch(fetchRepositories("%7Bquery%7D"));
+    }
+    if (query) {
+      dispatch(fetchRepositories(query));
+      console.log(query);
+    }
+  }, [query, dispatch]);
+
+  console.log("repositories");
+
+  return (
+    <div className="home-container">
+      <input
+        type="text"
+        placeholder="Search repositories..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <ul className="repo_ul">
+        {repositories.map((repo) => (
+          <li key={repo.id}>
+            <Link to={`/details/${repo.id}`}>{repo.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default HomeScreen;
